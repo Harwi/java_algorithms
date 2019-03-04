62acd481275ed1acf07c20c8c4087354591a339e
Konstantin <sh.konst89@gmail.com>
2019-03-03 20:34:36 -0800

    Dev

---
+++ source/src/repeated_string/Solution.java
@@ -0,0 +1,62 @@
+package repeated_string;
+
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.regex.*;
+
+public class Solution {
+
+    // Complete the repeatedString function below.
+    static long repeatedString(String s, long n) {
+        if (s == null || s.isEmpty()) {
+            throw new IllegalArgumentException("Input string must contain at least 1 character.");
+        }
+        if (n <= 0) {
+            throw new IllegalArgumentException("Input number of reps must exceed 0");
+        }
+        long len = s.length();
+        long reps = 0;
+        if (len <= n) {
+            reps = Math.floorDiv(n, len);
+        }
+        int cutOff = (int) (Math.floorMod(n, len));
+        long fullStrOccur = 0;
+        long cutOffStrOccur = 0;
+
+        for (int idx = 0; idx < s.length(); idx++) {
+            if (s.charAt(idx) == 'a') {
+                fullStrOccur++;
+                if (idx <= cutOff - 1 && cutOff > 0) {
+                    cutOffStrOccur++;
+                }
+            }
+        }
+
+        return reps * fullStrOccur + cutOffStrOccur;
+
+    }
+
+    private static final Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) throws IOException {
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        String s = scanner.nextLine();
+
+        long n = scanner.nextLong();
+        scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+        long result = repeatedString(s, n);
+
+        bufferedWriter.write(String.valueOf(result));
+        bufferedWriter.newLine();
+
+        bufferedWriter.close();
+
+        scanner.close();
+    }
+}
