736fec8826f1f4d1275836256066fb84fd1da88f
Konstantin <sh.konst89@gmail.com>
2019-07-06 12:30:27 -0700

    Dev

--- source/src/fraudent_activity/Solution.java
+++ source/src/fraudent_activity/Solution.java
@@ -11,39 +11,35 @@ import java.util.stream.Collectors;
 
 public class Solution {
 
-    static float calcMedian(List<Integer> input) {
-        if (input.size() == 0) {
-            return -1;
-        }
-        else if (input.size() == 1) {
-            return input.get(0);
-        }
-        else if (input.size() % 2 == 0) {
-            return (float) (input.get(input.size() / 2) + input.get(input.size() / 2 + 1)) / 2;
-        } else {
-            return input.get(input.size() / 2);
-        }
-    }
+    static float calcMedian(int[] freq) {
+//        if (input.size() == 0) {
+//            return -1;
+//        }
+//        else if (input.size() == 1) {
+//            return input.get(0);
+//        }
+//        else if (input.size() % 2 == 0) {
+//            return (float) (input.get(input.size() / 2) + input.get(input.size() / 2 + 1)) / 2;
+//        } else {
+//            return input.get(input.size() / 2);
+//        }
+        int[] prefix_sum = new int[201];
 
-    static Integer find(List<Integer> arr, Integer search) {
-        int l = 0, r = arr.size() - 1, m = 0;
-        while (l <= r) {
-            m = l + (r - l) / 2;
-
-            // Check if x is present at mid
-            if (arr.get(m).equals(search))
-                return m;
-
-            // If x greater, ignore left half
-            if (arr.get(m) < search)
-                l = m + 1;
-
-                // If x is smaller, ignore right half
-            else
-                r = m - 1;
+        prefix_sum[0] = freq[0];
+        for (int ix = 1; ix < freq.length; ix++) {
+            prefix_sum[ix] += prefix_sum[ix - 1];
         }
 
-        return m;
+        int total = prefix_sum[prefix_sum.length - 1];
+        int first_ix = prefix_sum[prefix_sum.length / 2];
+        int second_ix = prefix_sum[prefix_sum.length / 2 + 1];
+
+        int first_median;
+        int second_median;
+
+        for (int prefIx = 0; prefIx < prefix_sum.length; prefIx++) {
+
+        }
     }
 
     // Complete the activityNotifications function below.
@@ -53,23 +49,33 @@ public class Solution {
         }
 
         int notifications = 0;
+        // 0 <= x <= 200
+        int[] freq = new int[201];
+
         List<Integer> rangeArr = new ArrayList<>();
 
         for (int ix = d; ix < expenditure.length; ix++) {
             if (ix == d) {
-                rangeArr = Arrays.stream( Arrays.copyOfRange(expenditure, ix - d, ix) ).boxed().collect(Collectors.toCollection(ArrayList::new));
-                rangeArr.sort(Integer::compareTo);
-            } else {
-                int rangeIdxRemove = find(rangeArr, expenditure[ix - d - 1]);
-                rangeArr.remove(rangeIdxRemove);
-
-                if (rangeArr.get(0) > expenditure[ix - 1]) {
-                    rangeArr.add(0, expenditure[ix - 1]);
-                } else if (rangeArr.get(rangeArr.size() - 1) < expenditure[ix - 1]) {
-                    rangeArr.add(expenditure[ix - 1]);
-                } else {
-                    rangeArr.add(find(rangeArr, expenditure[ix - 1]) + 1, expenditure[ix - 1]);
+                for (int l = 0; l < ix; l++) {
+                    freq[expenditure[l]]++;
                 }
+
+//                rangeArr = Arrays.stream( Arrays.copyOfRange(expenditure, ix - d, ix) ).boxed().collect(Collectors.toCollection(ArrayList::new));
+//                rangeArr.sort(Integer::compareTo);
+            } else {
+                freq[expenditure[ix - d - 1]]--;
+                freq[expenditure[ix - 1]]++;
+
+//                int rangeIdxRemove = find(rangeArr, expenditure[ix - d - 1]);
+//                rangeArr.remove(rangeIdxRemove);
+//
+//                if (rangeArr.get(0) > expenditure[ix - 1]) {
+//                    rangeArr.add(0, expenditure[ix - 1]);
+//                } else if (rangeArr.get(rangeArr.size() - 1) < expenditure[ix - 1]) {
+//                    rangeArr.add(expenditure[ix - 1]);
+//                } else {
+//                    rangeArr.add(find(rangeArr, expenditure[ix - 1]) + 1, expenditure[ix - 1]);
+//                }
             }
 
             float median = calcMedian(rangeArr);
