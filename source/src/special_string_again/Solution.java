f65df0f565f7208a765749ac3c53fc8b14048436
Konstantin <sh.konst89@gmail.com>
2019-07-21 21:34:05 -0700

    Dev

--- source/src/special_string_again/Solution.java
+++ source/src/special_string_again/Solution.java
@@ -12,63 +12,24 @@ public class Solution {
 
     // Complete the substrCount function below.
     static long substrCount(int n, String s) {
-        long counter = 0;
-        long cntBeforeDelim = 0;
-        long cntAfterDelim = 0;
-        char repChar = s.charAt(0);
-        int delimIx = 0;
-        boolean canBePali = false;
-        for (int ix = 0; ix < n; ix++) {
-            if (!canBePali || s.charAt(ix) == s.charAt(ix - 1)) {
-                repChar = s.charAt(ix);
-                cntBeforeDelim++;
-            } else {
-                // There is enough space before end to build pali
-                if (!canBePali && ix + cntBeforeDelim < n) {
-                    canBePali = true;
-                    delimIx = ix;
-                }
-                if (s.charAt(ix) == repChar ) {
-                    cntAfterDelim++;
-                }
-                if (cntAfterDelim >= cntBeforeDelim) {
-                    canBePali = false;
-                    delimIx = 0;
-                }
-
-                if (s.charAt(ix) == repChar ) {
-                    cntAfterDelim++;
-
-
-                } else {
-                    if (cntBeforeDelim != cntAfterDelim) {
-                        canBePali = false;
-
-                    }
-                }
-
-
+        long result = 0;
+        for (int i = 0; i < n; i++) {
+            // if the current symbol is in the middle of palindrome, e.g. aba
+            long pali = 0;
+            int offset = 1;
+            while (i - offset >= 0 && i + offset < n && s.charAt(i - offset) == s.charAt(i + offset) && s.charAt(i - offset) == s.charAt(i - 1)) {
+                pali++;
+                offset++;
             }
+            // if this is repeatable characters aa
+            long repeats = 0;
+            while (i + 1 < n && s.charAt(i) == s.charAt(i + 1)) {
+                repeats++;
+                i++;
+            }
+            result += pali + (repeats * (repeats + 1) / 2);
         }
-
-//        for (int i = 0; i < n; i++) {
-//            // if the current symbol is in the middle of palindrome, e.g. aba
-//            int offset = 1;
-//            while (i - offset >= 0 && i + offset < n && s.charAt(i - offset) == s.charAt(i - 1)
-//                    && s.charAt(i + offset) == s.charAt(i - 1)) {
-//                counter++;
-//                offset++;
-//            }
-//            // if this is repeatable characters aa
-//            int repeats = 0;
-//            while (i + 1 < n && s.charAt(i) == s.charAt(i + 1)) {
-//                repeats++;
-//                i++;
-//            }
-//            counter += repeats * (repeats + 1) / 2;
-//        }
-//        return counter + n;
-
+        return result + n;
     }
 
     private static final Scanner scanner = new Scanner(System.in);
