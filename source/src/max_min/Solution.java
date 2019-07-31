2b9c4dc72fa26647577563ec5e93e8b68edb9238
Konstantin <sh.konst89@gmail.com>
2019-07-30 20:59:32 -0700

    Dev

---
+++ source/src/max_min/Solution.java
@@ -0,0 +1,51 @@
+package max_min;
+
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.regex.*;
+
+public class Solution {
+
+    // Complete the maxMin function below.
+    static int maxMin(int k, int[] arr) {
+        Arrays.sort(arr);
+        int min_k_diff = arr[arr.length - 1] - arr[0];
+        for (int ix = k - 1; ix < arr.length; ix++) {
+            min_k_diff = Math.min(min_k_diff, arr[ix] - arr[ix - (k - 1)]);
+        }
+        return min_k_diff;
+    }
+
+    private static final Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) throws IOException {
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        int n = scanner.nextInt();
+        scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+        int k = scanner.nextInt();
+        scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+        int[] arr = new int[n];
+
+        for (int i = 0; i < n; i++) {
+            int arrItem = scanner.nextInt();
+            scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+            arr[i] = arrItem;
+        }
+
+        int result = maxMin(k, arr);
+
+        bufferedWriter.write(String.valueOf(result));
+        bufferedWriter.newLine();
+
+        bufferedWriter.close();
+
+        scanner.close();
+    }
+}
