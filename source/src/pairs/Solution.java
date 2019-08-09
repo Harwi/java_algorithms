e029c48bdd52d6a4bda11be70ff043c90320457d
Konstantin <sh.konst89@gmail.com>
2019-08-08 22:08:47 -0700

    Dev

---
+++ source/src/pairs/Solution.java
@@ -0,0 +1,61 @@
+package pairs;
+
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.regex.*;
+
+public class Solution {
+
+    // Complete the pairs function below.
+    static int pairs(int k, int[] arr) {
+        Arrays.sort(arr);
+        int res = 0;
+        for (int ix = 0; ix < arr.length; ix++) {
+            if (arr[ix] >= k) {
+                int bIx = ix - 1;
+                while (bIx >= 0 && arr[ix] - arr[bIx] <= k) {
+                    if (arr[ix] - arr[bIx] == k) {
+                        res++;
+                    }
+                    bIx--;
+                }
+            }
+        }
+        return res;
+    }
+
+    private static final Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) throws IOException {
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        String[] nk = scanner.nextLine().split(" ");
+
+        int n = Integer.parseInt(nk[0]);
+
+        int k = Integer.parseInt(nk[1]);
+
+        int[] arr = new int[n];
+
+        String[] arrItems = scanner.nextLine().split(" ");
+        scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+        for (int i = 0; i < n; i++) {
+            int arrItem = Integer.parseInt(arrItems[i]);
+            arr[i] = arrItem;
+        }
+
+        int result = pairs(k, arr);
+
+        bufferedWriter.write(String.valueOf(result));
+        bufferedWriter.newLine();
+
+        bufferedWriter.close();
+
+        scanner.close();
+    }
+}
