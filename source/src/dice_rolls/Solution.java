70bb82c975098d8997e4f24426e5dde0deed603f
konstantin <sh.konst89@gmail.com>
2019-10-07 08:57:33 -0700

    Dev

---
+++ source/src/dice_rolls/Solution.java
@@ -0,0 +1,13 @@
+package dice_rolls;
+
+class Solution {
+
+
+    // d - num of dice (1..d)
+    // f - num of faces (1..f)
+    // target = value to get
+    public int numRollsToTarget(int d, int f, int target) {
+        
+
+    }
+}
