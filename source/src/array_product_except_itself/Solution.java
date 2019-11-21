da3eba580bf7e80df0c33383b08ebdd1ff05dd20
konstantin <sh.konst89@gmail.com>
2019-11-20 19:17:48 -0800

    Dev

---
+++ source/src/array_product_except_itself/Solution.java
@@ -0,0 +1,24 @@
+package array_product_except_itself;
+
+public class Solution {
+
+    // Solve without division.
+    public int[] productExceptSelf(int[] nums) {
+        int[] res = new int[nums.length];
+        int prodLeft = 1;
+        for (int i = 0; i < res.length; i++) {
+            res[i] = nums[i] * prodLeft;
+            prodLeft = res[i];
+        }
+        int prodRight = 1;
+        for (int i = res.length - 1; i >= 0; i--) {
+            if (i > 0) {
+                res[i] = res[i - 1] * prodRight;
+                prodRight = prodRight * nums[i];
+            } else {
+                res[i] = prodRight;
+            }
+        }
+        return res;
+    }
+}
---
+++ source/src/array_reorder_logs/Solution.java
@@ -0,0 +1,59 @@
+package array_reorder_logs;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.List;
+import java.util.stream.Stream;
+
+public class Solution {
+    private static final String SPACE = " ";
+
+    public String[] reorderLogFiles(String[] logs) {
+        List<String> letters = new ArrayList<>();
+        List<String> digits = new ArrayList<>();
+
+        for (String log : logs) {
+            int delimIx = log.indexOf(SPACE) + 1;
+            if (Character.isDigit(log.charAt(delimIx))) {
+                digits.add(log);
+            } else {
+                letters.add(log);
+            }
+        }
+
+        Collections.sort(letters, new LogComparator());
+
+        return Stream.concat(letters.stream(), digits.stream()).toArray(String[]::new);
+    }
+
+    class LogComparator implements Comparator<String> {
+        public int compare(String str1, String str2) {
+            int ix1 = str1.indexOf(SPACE) + 1;
+            int ix2 = str2.indexOf(SPACE) + 1;
+
+            while (ix1 < str1.length() && ix2 < str2.length()) {
+                if (str1.charAt(ix1) == str2.charAt(ix2)) {
+                    ix1++;
+                    ix2++;
+                } else {
+                    return str1.charAt(ix1) - str2.charAt(ix2);
+                }
+            }
+
+            ix1 = 0;
+            ix2 = 0;
+
+            while (ix1 < str1.indexOf(SPACE) && ix2 < str2.indexOf(SPACE)) {
+                if (str1.charAt(ix1) == str2.charAt(ix2)) {
+                    ix1++;
+                    ix2++;
+                } else {
+                    return str1.charAt(ix1) - str2.charAt(ix2);
+                }
+            }
+
+            return 0;
+        }
+    }
+}
